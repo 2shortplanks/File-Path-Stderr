450ff7ac88195c0aea424c73e73bf19e74f825c1
Mark Fowler <mark@twoshortplanks.com>
2012-02-22 08:56:30 +0000

    start work on 2.0; port to Module::Install, add kwalitee tests, fix broken tests.  Refactor way we do the wrapping of functions so they return the right thing

--- lib/File/Path/Stderr.pm
+++ lib/File/Path/Stderr.pm
@@ -1,5 +1,7 @@
 package File::Path::Stderr;
 
+use 5.005;  # yes, really
+
 use strict;
 #use warnings;
 
@@ -8,7 +10,7 @@ use File::Path ();
 require Exporter;
 @File::Path::Stderr::ISA = qw(Exporter);
 @File::Path::Stderr::EXPORT = qw(mkpath rmpath);
-$File::Path::Stderr::VERSION = "1.00";
+$File::Path::Stderr::VERSION = "2.00";
 
 =head1 NAME
 
@@ -18,6 +20,7 @@ File::Path::Stderr - like File::Path but print to STDERR
 
    use File::Path::Stderr;
 
+   # standard File::Path v1 interface
    mkpath(['/foo/bar/baz', 'blurfl/quux'], 1, 0711);
    rmtree(['foo/bar/baz', 'blurfl/quux'], 1, 1);
 
@@ -29,29 +32,48 @@ except rather than printing activity reports to the currently selected
 filehandle (which is normally STDOUT) the messages about what B<File::Path>
 is doing are printed to STDERR.
 
+=head2 Functions
+
+The following functions from File::Path are currently supported:
+
+=over
+
+=item mkpath
+
+=item rmpath
+
+=back
+
 =cut
 
-sub mkpath
-{
-  my $old = select();
-  select(STDERR);
-  File::Path::mkpath(@_);
-  select($old);
+sub File::Path::Stderr::End::DESTORY {
+  my $self = shift;
+  return $self->();
 }
 
-sub rmpath
-{
+sub _with_stdderr(&) {
+  # remember what file handle was selected
   my $old = select();
+
+  # select STDERR instead
   select(STDERR);
-  File::Path::rmpath(@_);
-  select($old);
+
+  # after we've returned (but before the next statement)
+  # switch the file handle back to what it was before
+  my $run_on_destroy = bless sub { select($old) }, "File::Path::Stderr::End";
+
+  # run the
+  return $_[0]->();
 }
+
+sub mkpath { return _with_stderr { File::Path::mkpath(@_); } }
+sub rmpath { return _with_stderr { File::Path::rmpath(@_); } }
 
 =head1 AUTHOR
 
 Written by Mark Fowler E<lt>mark@twoshortplanks.comE<gt>
 
-Copryright Mark Fowler 2003.  All Rights Reserved.
+Copryright Mark Fowler 2003, 2012.  All Rights Reserved.
 
 This program is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.
